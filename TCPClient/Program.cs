35269c1a7a7cd96212619930386c2d09112e3b43
Ramiro0318 <ramiro.val03@gmail.com>
2026-02-25 17:49:21 -0600

    Nuevo proyecto de conexiión TCP. Servidor TCP. Cliente TCP.

---
+++ TCPClient/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Endpoint remoto
+
+using System.Net;
+using System.Net.Mime;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Ip del servidor:");
+var ip = Console.ReadLine();
+
+IPEndPoint remoto = new IPEndPoint(IPAddress.Parse(ip ?? "0.0.0.0"), 12600);
+TcpClient tcpClient = new TcpClient(remoto); //Connection request
+
+Console.WriteLine("Escribe un mensaje");
+string mensaje;
+while ((mensaje = Console.ReadLine()) != null)
+{
+    byte[] buffer = Encoding.UTF8.GetBytes(mensaje);
+    var stream = tcpClient.GetStream();
+    stream.Write(buffer, 0, buffer.Length);
+
+
+    Console.WriteLine("Mensaje enviado");
+}
+
+Console.WriteLine("Mensaje enviado");
---
+++ TCPServer/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+List<TcpClient> Clients = new List<TcpClient>();
+TcpListener servidor = new TcpListener(IPAddress.Any, 12600);
+
+servidor.Start(); //Inicia a escuchar por peticiones
+
+Console.WriteLine("Servidor TCP iniciado, escuchando en el puerto 12600");
+
+while (true)
+{
+    var cliente = servidor.AcceptTcpClient(); //Espera a que un cliente se acepte
+
+    Clients.Add(cliente);   //Agregarlo a la lista
+    Console.WriteLine("Conexión aceptada con: " + cliente.Client?.RemoteEndPoint ?? "");
+
+    Thread hilo = new Thread(EscucharCliente);
+    hilo.IsBackground = true;
+    hilo.Start(cliente);
+
+}
+
+void EscucharCliente(object? c)
+{
+    if (c != null)
+    {
+        TcpClient tcpClient = (TcpClient)c; //Unboxing
+        while (tcpClient.Connected)
+        {
+            var stream = tcpClient.GetStream();
+            if (tcpClient.Available > 0)
+            {
+                byte[] buffer = new byte[tcpClient.Available];
+                stream.ReadExactly(buffer, 0, buffer.Length);
+                var mensaje = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine(tcpClient.Client.RemoteEndPoint + "Dice esto: " + mensaje);
+            }
+            Thread.Sleep(100);
+        }
+        Clients.Remove(tcpClient);
+
+    }
+}
