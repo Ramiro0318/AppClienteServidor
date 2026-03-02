b416437e771f752f77a66b4b78c0defd1f53a38f
Ramiro0318 <ramiro.val03@gmail.com>
2026-03-02 17:50:48 -0600

    Modelos y de cliente y los comandos.

---
+++ AhorcadoBattleRoyal/AhorcadoServer/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhorcadoServer.Models
+{
+    public class Cliente
+    {
+        public TcpListener? Servidor;
+        public List<Cliente> Clientes { get; set; } = [];
+
+        int puertoEscucha = 7777;
+
+        private string? fraseAdivinar;
+        public int Errores;
+        public Cliente Turno { get; set; }
+        public int Ronda { get; set; }
+
+        public string? FraseOculta { get; set; }
+        public List<string> LetrasDisponibles { get; set; }
+    }
+}
---
+++ AhorcadoBattleRoyal/AhorcadoServer/Models/Comandos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhorcadoServer.Models
+{
+    public enum Orden { Conectar, Bienvenido, Rechazar, CambiarTurno, Responder, Expulsar, Ganar, CambiarRonda }
+    public class Comandos
+    {
+        public string Comando { get; set; } = null!;
+    }
+
+    public class ConectarComand : Comandos
+    {
+        public string Nombre { get; set; } = null!;
+    }
+
+    public class BienvenidoComando : Comandos
+    {
+        public List<string> Nombres { get; set; } = [];
+    }
+
+    public class RechazarComando : Comandos { }
+
+    public class TurnoComando : Comandos
+    {
+        public string? Palabra { get; set; }
+        public string? JugadorTurno { get; set; } = null!;
+        public int NumErrores { get; set; }
+        public List<string> LetrasDisponibles { get; set; } = [];
+    }
+
+    public class ResponderComando : Comandos
+    {
+        public string? Letra { get; set; }
+    }
+
+    public class ExpulsarComando : Comandos
+    {
+        public string? Jugador { get; set; }
+        public string? Palabra { get; set; }
+    }
+
+    public class GanadorComando : Comandos
+    {
+        public string? Jugador { get; set; }
+    }
+
+    public class CambiarRondaComand : Comandos { }
+}
