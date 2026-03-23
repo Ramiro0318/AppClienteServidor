e6657eeffaaec5e9669dcfb98be0914645805dcd
Ramiro0318 <ramiro.val03@gmail.com>
2026-03-23 17:48:44 -0600

    Creacion de ejemplo http

---
+++ EjemploHTTP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+string url = "http://*:8081/ejemplo";
+HttpListener servidorhttp = new();
+
+servidorhttp.Prefixes.Add(url);
+servidorhttp.Start();
+
+while (true)
+{
+    var contexto = servidorhttp.GetContext();
+}
+
+Console.WriteLine("Hello, World!");
+
+
