4f119fa67bd3ff9041f3289adf391f5a34e2465f
Ramiro0318 <ramiro.val03@gmail.com>
2026-03-24 17:48:30 -0600

    Proyecto para recibir y enviar mensajes mediatne pagina html

--- EjemploHTTP/Program.cs
+++ EjemploHTTP/Program.cs
@@ -1,16 +1,43 @@
-﻿using System.Net;
+﻿using System.Diagnostics.Contracts;
+using System.Net;
+using System.Text;
 
-string url = "http://*:8081/ejemplo";
+string url = "http://localhost:8081/ejemplo/";
+//string url = "http://*/ejemplo/";
 HttpListener servidorhttp = new();
 
 servidorhttp.Prefixes.Add(url);
 servidorhttp.Start();
 
+var index = File.ReadAllText("index.html");
+byte[] buffer = Encoding.UTF8.GetBytes(index);
+
 while (true)
 {
-    var contexto = servidorhttp.GetContext();
+    var contexto = servidorhttp.GetContext();  //El contexto es la combinacion entre una petición y una respuesta
+                                               //Bloquea el hilo
+    if (contexto.Request.HttpMethod == "GET")
+    {
+        EnviarIndex(buffer, contexto);
+    }
+    else if (contexto.Request.HttpMethod == "POST")
+    {
+        StreamReader streamReader = new StreamReader
+            (contexto.Request.InputStream);
+        var texto = streamReader.ReadToEnd();
+
+        Console.WriteLine(texto);
+        Console.WriteLine("Hello, World!");
+        EnviarIndex(buffer, contexto);
+    }
+
 }
 
-Console.WriteLine("Hello, World!");
-
-
+static void EnviarIndex(byte[] buffer, HttpListenerContext contexto)
+{
+    contexto.Response.StatusCode = 200;
+    contexto.Response.ContentLength64 = buffer.Length;
+    contexto.Response.ContentType = "text/html";
+    contexto.Response.OutputStream.Write(buffer, 0, buffer.Length);
+    contexto.Response.Close();
+}
