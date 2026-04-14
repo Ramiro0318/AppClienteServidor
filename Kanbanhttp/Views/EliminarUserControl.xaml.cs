1ae1abb9f31bb44ca36ec98a508d17eca376ff26
Ramiro0318 <ramiro.val03@gmail.com>
2026-04-14 17:53:55 -0600

    Añadido el Programa a la solución correcta.

---
+++ Kanbanhttp/Views/EliminarUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kanbanhttp.Views
+{
+    /// <summary>
+    /// Interaction logic for EliminarUserControl.xaml
+    /// </summary>
+    public partial class EliminarUserControl : UserControl
+    {
+        public EliminarUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
