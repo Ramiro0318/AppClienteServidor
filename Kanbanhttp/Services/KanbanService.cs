4ad70f4445711a05f2cd207492fcf6bdc9c74165
Ramiro0318 <ramiro.val03@gmail.com>
2026-04-16 17:51:21 -0600

    Serializar y deserializar Tareas,. metodo para atender request.

--- Kanbanhttp/Services/KanbanService.cs
+++ Kanbanhttp/Services/KanbanService.cs
@@ -1,10 +1,12 @@
 ﻿using Kanbanhttp.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json;
 
 namespace Kanbanhttp.Services
 {
@@ -17,6 +19,7 @@ namespace Kanbanhttp.Services
         {
             string url = "http://*:8080/kanban/";
             servidor.Prefixes.Add(url);
+
         }
 
         public void Iniciar()
@@ -39,14 +42,74 @@ namespace Kanbanhttp.Services
 
             //Atender
 
+            //Regresar archivos (index.html estilos.css, script.js)
+
+            //Regresar el tablero (/tablero = index.html)
+
+            //Regresar los datos GET(/tareas)
+
+            //Camniar la tarea put (/tareas)
+
+            var request = context.Request;
+            var response = context.Response;
+
+            if (request.HttpMethod == "GET" && request.RawUrl == "/tablero")
+            {
+                byte[]? buffer = LeerRecurso("index.html");
+                Enviar(response, buffer, "text/html");
+            }
+            else if (request.HttpMethod == "GET" && request.RawUrl == "/tareas")
+            {
+                string json = "";
+                lock (Tareas)
+                {
+                    json = JsonSerializer.Serialize(GetAll());
+                }
+
+                var buffer = Encoding.UTF8.GetBytes(json);
+                Enviar(response, buffer, "aplication/json");
+                
+            }
+            response.Close();
+
         }
 
-        public event Action<Tarea>? TareaCreada;
-        public event Action<Tarea>? TareaEliminada;
+        private static void Enviar(HttpListenerResponse response, byte[]? buffer, string type)
+        {
+            if (buffer != null)
+            {
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.ContentLength64 = buffer.Length;
+                response.ContentType = "text/html";
+                response.StatusCode = 200;
+            }
+            else
+            {
+                response.StatusCode = 404;
+            }
+        }
+
+        byte[]? LeerRecurso(string nombre)
+        {
+            try
+            {
+                byte[] buffer = File.ReadAllBytes("/Assets/" + nombre);
+                return buffer;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+        }
+
+        public event Action<Tarea>? TareaCreada, TareaModificada, TareaEliminada;
         public void CrearTarea(Tarea t)
         {
             lock (Tareas)
             {
+                t.Estado = Estados.Pendiente;
                 t.Id = Tareas.Max(x => x.Id) + 1;
                 Tareas.Add(t);
                 Serializar();
@@ -73,9 +136,37 @@ namespace Kanbanhttp.Services
             }
         }
 
+        public void CambiarEstado(Tarea t)
+        {
+            if (t.Estado != Estados.Finalizado) return;
+            //t.Estado = t.Estado + 1;
+            t.Estado++;
+            Serializar();
+            TareaModificada?.Invoke(t);
+        }
+
+        public void CambiarEstadoAdmin(Tarea t, Estados estado)
+        {
+            t.Estado = estado;
+            Serializar();
+            TareaModificada?.Invoke(t);
+        }
+
         void Serializar()
         {
+            lock (Tareas)
+            {
+                File.WriteAllText("kanban.json", JsonSerializer.Serialize(Tareas));
+            }
+        }
 
+        void Deserializar()
+        {
+            lock (Tareas)
+            {
+                Tareas?.AddRange(JsonSerializer.Deserialize<List<Tarea>>(File.ReadAllText("kanban.json")) ?? []);
+                Tareas?.ForEach(x => TareaCreada?.Invoke(x));
+            }
         }
 
         public void Detener()
