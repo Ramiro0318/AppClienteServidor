658c8d989671f126488edc6de41fd8ff2b03a7b2
Ramiro0318 <ramiro.val03@gmail.com>
2026-04-23 17:51:46 -0600

    Programacion del cliente, drag and drop.

--- Kanbanhttp/Assets/script.js
+++ Kanbanhttp/Assets/script.js
@@ -1,8 +1,11 @@
-﻿
+﻿if (!localStorage.getItem("nombreUsuario")) {
+    let nombre = prompt("¿Cual es tu nombre de usuario?");
+    localStorage.setItem("nombreUsuario" nombre);
+}
+
 let tareas = [];
 
-async function descargarTaras()
-{
+async function descargarTaras() {
     let response = await fetch("/taras");
 
     if (response.ok) {
@@ -15,14 +18,50 @@ async function descargarTaras()
 
 }
 
-function dibujarObjetos()
-{
 
+let template = document.querySelector("template");
+let columnas = document.querySelectorAll("tbody td");
+
+function dibujarObjetos() {
+    columnas.forEach(x => x.replaceChildren());
+
+    for (let tarea of tareas) {
+        let clon = template.content.cloneNode(true);
+
+        clon.firstElementChild.children[0].innerText = tarea.Usuario;
+        clon.firstElementChild.children[1].innerText = tarea.Descripcion;
+        clon.firstElementChild.children[2].innerText = tarea.Fecha;
+        clon.firstElementChild.dataset.id = tarea.id;
+
+        columnas[tarea.Estado].append(clon);
+
+    }
+    setTimeout(descargarTaras(), 3000)
 }
+
 
 
 descargarTaras();
 
+let postitMoviendo;
+document.querySelector("tbody").addEventListener("dragstart", function (e) {
+
+    if (e.target.tagName == "DIV") {
+        postitMoviendo = e.target;
+
+    }
+});
+
+document.querySelector("tbody").addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
 
 
+document.querySelector("tbody").addEventListener("drop", function (e) {
+    e.preventDefault();
+
+    if (e.target.tagName == "TD") {
+        e.target.append(postitMoviendo);
+    }
+});
 
